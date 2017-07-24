456d19d2c7ede8a98b33f624a40ae8627ab9676b
Connor Linfoot <connorlinfoot@me.com>
2017-07-24 20:26:14 +0100

    ok i wanted it to do more

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ function show_help() {
     console.log('Options:');
     console.log('   -h      Shows this message');
     console.log('   -u      The URL to redirect users too');
-    console.log('   -p      Ports to listen on, separated by commas');
+    console.log('   -ur     The URL to redirect users too using regex to support multiple URL\'s'); // Example of this is "node index -ur CratesPlus=https://github.com/ConnorLinfoot/CratesPlus,https://github.com/ConnorLinfoot -p 80,8080"
+    console.log('   -p      Ports to listen on, separated by commas, defaults to 80');
+    console.log('   -c      Code to redirect with, defaults to 301');
     process.exit(0);
 }
 
-
 // Process args
-const args = {};
+const args = {
+    ports: 80,
+    code: 301
+};
+
 if( process.argv.slice(2).length == 0 ) {
     show_help();
 }
@@ -32,26 +37,59 @@ for( let arg of process.argv.slice(2) ) {
         case '-url':
             next_var = 'url';
             break;
+        case '-ur':
+        case '-urls':
+        case '-urlregex':
+        case '-urlsregex':
+            next_var = 'urls';
+            break;
         case '-port':
         case '-ports':
         case '-p':
             next_var = 'ports';
             break;
+        case '-code':
+        case '-c':
+            next_var = 'code';
+            break;
     }
 }
 
-if( args.url == undefined || args.url == null ) {
-    console.error('No URL specified!');
+if( (args.url == undefined || args.url == null) && args.urls == undefined || args.urls == null ) {
+    console.error('No URL(s) specified!');
     process.exit(0);
 } else if( args.ports == undefined || args.ports == null ) {
     console.error('No port(s) specified!');
     process.exit(0);
 }
 
+if( args.urls != undefined && args.urls != null ) {
+    args.urls = args.urls.split(',');
+}
+
+args.code = parseInt(args.code);
+
 function handle_redirect(req, res) {
+    let url = args.url;
+    if( args.urls != undefined && args.urls != null ) {
+        for( let regex_url of args.urls ) {
+            if( regex_url.split('=').length == 1 ) {
+                url = regex_url;
+                break;
+            }
+
+            const regex = new RegExp(regex_url.split('=')[0], 'i');
+            if( regex.test(req.url) ) {
+                url = regex_url.split('=')[1];
+                break;
+            }
+
+        }
+    }
+
     console.log('');
-    console.log('Redirecting ' + req.url + ' to ' + args.url + ' (IP: ' + req.connection.remoteAddress + ')');
-    res.redirect(301, args.url);
+    console.log('Redirecting ' + req.url + ' to ' + url + ' (IP: ' + req.connection.remoteAddress + ')');
+    res.redirect(args.code, url);
 }
 
 const app = express();
